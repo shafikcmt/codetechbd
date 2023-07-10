2b5180c999e5e57122ebf0935a57e59f1061c20b
shafikcmt <mdshafiqulislam822@gmail.com>
2023-07-10 06:24:58 +0530

    AdminSeeder file updated

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -22,7 +22,7 @@ class AdminSeeder extends Seeder
             'phone'             => '01732805218',
             'designation'       => 'Web Application Developer',
             'email_verified_at' => now(),
-            'password'          => Hash::make('123123123'),
+            'password'          => Hash::make('Admin123@#'),
             'remember_token'    => Str::random(10),
         ]);
     }
